c9b8be03a9d4da0c22d81daec2f925e86d972f9b
Famouus Bernard <famousbernard@gmail.com>
2023-07-04 15:31:29 -0700

    simply match school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-# Get the argument passed to the script
-input = ARGV[0]
-
-# Define the regular expression to match against
-regex = Oniguruma::ORegexp.new('School')
-
-# Use the regular expression to match against the argument
-if regex.match(input)
-  puts "The string '#{input}' matches the regular expression 'School'"
-else
-  puts "The string '#{input}' does not match the regular expression 'School'"
-end
+puts ARGV[0].scan(/School/).join
