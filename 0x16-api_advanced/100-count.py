82f4b8aca0c076c96cfdeaff129d441a4412eeb3
Famouus Bernard <famousbernard@gmail.com>
2023-10-10 21:11:20 +0000

    successful update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Function to count words in all
-hot posts of a given Reddit subreddit."""
+"""
+A recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords.
+"""
 
 import json
 import requests
