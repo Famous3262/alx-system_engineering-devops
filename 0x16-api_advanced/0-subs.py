298e7c14ce21e3a223acbc14f9621e3123d3ab5e
Famouus Bernard <famousbernard@gmail.com>
2023-10-10 12:25:55 +0000

    num of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-A function that returns the number of subscribers for a given subreddit
+A function that returns the number
+of subscribers for a given subreddit
 """
 
 import requests
 
 def number_of_subscribers(subreddit):
+"""Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+			    allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
