889293a56f8f54bc4a13bbbedf5c3f0ceadc0c46
Famouus Bernard <famousbernard@gmail.com>
2023-10-11 03:08:24 +0000

    Strace is a friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
